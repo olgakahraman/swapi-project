8b01da00c6633e7fe86e87728f2defa938d272ee
olgakahraman <ol.khrmn@gmail.com>
2024-03-01 16:13:19 -0500

    added event listener to buttons

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const results = document.querySelector("#results");
+
+
+
+
+// added Event Listener to buttons in navbar
+document.querySelector("#pillNav2").addEventListener("click", (e) => {
+  asyncFetch(e.target.textContent.trim().toLowerCase());
+});
