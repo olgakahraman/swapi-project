4a2a149ef9a878998cb5d1bce2875027561ec769
olgakahraman <ol.khrmn@gmail.com>
2024-03-01 16:14:16 -0500

    created a asyncFetch function to integrate API to project

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const results = document.querySelector("#results");
 
-
+async function asyncFetch(value) {
+  const res = await fetch(`https:swapi.tech/api/${value}/`);
+  const data = await res.json();
+  displayResults(data, value);
+}
 
 
 // added Event Listener to buttons in navbar
