9c02b48dfe59120586396f8fc6a435beef776171
olgakahraman <ol.khrmn@gmail.com>
2024-03-04 16:01:04 -0500

    created displayResults() to show results

--- index.js
+++ index.js
@@ -7,6 +7,41 @@ async function asyncFetch(value) {
 }
 
 
+function displayResults(data, value) {
+  let output = "";
+  console.log(data);
+  if (value === "people") {
+    data.results.forEach((item) => {
+      output += `
+      <div class="card p-3 m-3" >
+      <h4 class = "card-title text-center">${item.name}</h4>
+      </div>
+      `;
+    });
+  }
+  if (value === "planets") {
+    data.results.forEach((item) => {
+      output += `
+      <div class="card p-3 m-3" >
+      <h4 class = "card-title text-center">${item.name}</h4>
+      </div>
+      `;
+    });
+  }
+  if (value === "starships") {
+    data.results.forEach((item) => {
+      output += `
+      <div class="card p-3 m-3">
+      <h4 class = "card-title text-center">${item.name}</h4>
+      </div>
+      `;
+    });
+  }
+
+  results.innerHTML = output;
+}
+
+
 // added Event Listener to buttons in navbar
 document.querySelector("#pillNav2").addEventListener("click", (e) => {
   asyncFetch(e.target.textContent.trim().toLowerCase());
