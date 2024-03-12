1a846d5f3aa783be7c8cc4ec5661c0e2b66ef819
olgakahraman <ol.khrmn@gmail.com>
2024-03-12 13:33:50 -0400

    added loading() for  loading page

--- index.js
+++ index.js
@@ -3,19 +3,26 @@ let darkMode = localStorage.getItem("darkMode");
 const toggleBtn = document.querySelector("#toggleBtn");
 const filmRequest = "https:swapi.tech/api/films/";
 
+function loading() {
+  results.innerHTML = '<i class="loading fas fa-circle-notch fa-spin fa-sw"></i>';
+}
+
 //  we will grub the filmsrequest and retrieve the data and dsplay films
 fetch(filmRequest)
+
   .then((response) => {
     return response.json();
+   
   })
   .then((data) => {
     let output = `<div class="row">`;
     let films = document.querySelector("#films");
-
+   
     console.log(data);
     data.result.forEach((item) => {
+     
       output += `
-      <div class="col-4" id="cardElement">
+      <div class="col-md-4" id="cardElement">
       <div class="card p-3 m-3 " >
       <h5 class = "card-title text-center"><strong>${item.properties.title}</strong></h5>
       <br />
@@ -25,8 +32,10 @@ fetch(filmRequest)
       </div></div>
       `;
     });
+    
     output = output + `</div>`;
     films.innerHTML = output;
+    
   });
 
 // we will grub the link to retrieve the data and display information depening on value
@@ -38,14 +47,14 @@ async function asyncFetch(value) {
 }
 
 function displayResults(data, value) {
-  let output = `<div class="row">`;
+  let output = `<div class="row ">`;
   let header = document.querySelector("#header");
   console.log(data);
   if (value === "people") {
     header = `<h1 class="text-center" id="#header"><i class="fa-solid fa-people-group rotating"></i> ${value}</h1>`;
     data.results.forEach((item) => {
       output += `
-      <div class="col-4" id="cardElement">
+      <div class="col-md-4 " id="cardElement">
       <div class="card p-3 m-3 " >
       <h6 class = "card-title text-center">${item.name}</h6>
 
@@ -57,7 +66,7 @@ function displayResults(data, value) {
     header = `<h1 class="text-center" id="#header"><i class="fa-solid fa-earth-americas rotating"></i> ${value}</h1>`;
     data.results.forEach((item) => {
       output += `
-      <div class="col-4" id="cardElement">
+      <div class="col-md-4" id="cardElement">
       <div class="card p-3 m-3" >
       <h6 class = "card-title text-center">${item.name}</h6>
 
@@ -70,7 +79,7 @@ function displayResults(data, value) {
     header = `<h1 class="text-center" id="#header"><i class="fa-solid fa-shuttle-space rotating"></i> ${value}</h1>`;
     data.results.forEach((item) => {
       output += `
-      <div class="col-4" id="cardElement">
+      <div class="col-md-4" id="cardElement">
       <div class="card p-3 m-3">
       <h6 class = "card-title text-center">${item.name}</h6>
       </div></div>
@@ -81,19 +90,16 @@ function displayResults(data, value) {
   }
 
   output = header + output + `</div>`;
-
   results.innerHTML = output;
 }
 
 const enableDarkMode = () => {
   document.body.classList.add("dark");
-
   localStorage.setItem("darkMode", "enabled");
 };
 
 const disableDarkMode = () => {
   document.body.classList.remove("dark");
-
   localStorage.setItem("darkMode", null);
 };
 
@@ -114,16 +120,13 @@ toggleBtn.addEventListener("click", () => {
 
     toggleBtn.classList.remove("toggled");
     body.classList.remove("dark");
-
     localStorage.setItem("darkMode", null);
 
     lightenText.classList.remove("unselected");
     darkenText.classList.add("unselected");
   } else {
     toggleBtn.classList.add("toggled");
-
     body.classList.add("dark");
-
     localStorage.setItem("darkMode", "unselected");
     enableDarkMode();
     lightenText.classList.add("unselected");
@@ -134,4 +137,5 @@ toggleBtn.addEventListener("click", () => {
 // added Event Listener to buttons in navbar to show value to asyncFetch function
 document.querySelector("#pillNav2").addEventListener("click", (e) => {
   asyncFetch(e.target.textContent.trim().toLowerCase());
+  loading();
 });
